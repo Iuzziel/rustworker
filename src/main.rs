df4de6df90c82a269239a5a711f6016012c8dc8c
iuzziel <iuzziel.abrudavah@outlook.com>
2019-12-25 17:11:46 +0100

    Learning from exemples

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,100 @@
 
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+extern crate chrono;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[cfg(test)]
+mod tests;
+
+use std::collections::HashMap;
+use std::sync::Mutex;
+
+use chrono::prelude::*;
+use rocket::State;
+use rocket_contrib::json::{Json, JsonValue};
+
+// The type to represent the ID of a message.
+type ID = usize;
+// We're going to store all of the messages here. No need for a DB.
+type MessageMap = Mutex<HashMap<ID, String>>;
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    id: Option<ID>,
+    contents: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct TimeMessage {
+    hour: String,
+    date: String,
+}
+
+#[post("/<id>", format = "json", data = "<message>")]
+fn new(id: ID, message: Json<Message>, map: State<MessageMap>) -> JsonValue {
+    let mut hashmap = map.lock().expect("map lock.");
+    if hashmap.contains_key(&id) {
+        json!({
+            "status": "error",
+            "reason": "ID exists. Try put."
+        })
+    } else {
+        hashmap.insert(id, message.0.contents);
+        json!({ "status": "ok" })
+    }
+}
+
+#[put("/<id>", format = "json", data = "<message>")]
+fn update(id: ID, message: Json<Message>, map: State<MessageMap>) -> Option<JsonValue> {
+    let mut hashmap = map.lock().unwrap();
+    if hashmap.contains_key(&id) {
+        hashmap.insert(id, message.0.contents);
+        Some(json!({ "status": "ok" }))
+    } else {
+        None
+    }
+}
+
+#[get("/<id>", format = "json")]
+fn get(id: ID, map: State<MessageMap>) -> Option<Json<Message>> {
+    let hashmap = map.lock().unwrap();
+    hashmap.get(&id).map(|contents| {
+        Json(Message {
+            id: Some(id),
+            contents: contents.clone(),
+        })
+    })
+}
+
+#[get("/time")]
+fn get_time() -> JsonValue {
+    let utc: DateTime<Utc> = Utc::now();
+    json!(TimeMessage {
+        hour: utc.format("%H:%M:%S").to_string(),
+        date: utc.format("%Y-%m-%d").to_string(),
+    })
+}
+
+#[catch(404)]
+fn not_found() -> JsonValue {
+    json!({
+        "status": "error",
+        "reason": "Resource was not found."
+    })
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/message", routes![new, update, get])
+        .mount("/", routes![get_time])
+        .register(catchers![not_found])
+        .manage(Mutex::new(HashMap::<ID, String>::new()))
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
 }
